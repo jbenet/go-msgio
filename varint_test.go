864f9ed55df3b43514e6d2e36429156e413d40f8
Juan Batiz-Benet <juan@benet.ai>
2014-12-29 07:21:01 -0800

    test varint write specifically

--- varint_test.go
+++ varint_test.go
@@ -2,6 +2,7 @@ package msgio
 
 import (
 	"bytes"
+	"encoding/binary"
 	"testing"
 )
 
@@ -24,4 +25,31 @@ func TestVarintReadWriteMsgSync(t *testing.T) {
 	writer := NewVarintWriter(buf)
 	reader := NewVarintReader(buf)
 	SubtestReadWriteMsgSync(t, writer, reader)
+}
+
+func TestVarintWrite(t *testing.T) {
+
+	SubtestVarintWrite(t, []byte("hello world"))
+	SubtestVarintWrite(t, []byte("hello world hello world hello world"))
+	SubtestVarintWrite(t, make([]byte, 1<<20))
+}
+
+func SubtestVarintWrite(t *testing.T, msg []byte) {
+	buf := bytes.NewBuffer(nil)
+	writer := NewVarintWriter(buf)
+
+	if err := writer.WriteMsg(msg); err != nil {
+		t.Fatal(err)
+	}
+
+	sbr := simpleByteReader{R: buf}
+	n, err := binary.ReadUvarint(&sbr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("checking varint is %d", len(msg))
+	if int(n) != len(msg) {
+		t.Fatalf("incorrect varint: n != %d", len(msg))
+	}
 }
